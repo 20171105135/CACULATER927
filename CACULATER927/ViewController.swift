9261c440d57c85d72b52972be32d1f4df0325431
20171105135 <1115828778@qq.com>
2018-09-27 09:51:31 +0800

    计算机程序
    加法计算

--- CACULATER927/ViewController.swift
+++ CACULATER927/ViewController.swift
@@ -8,8 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController:
+UIViewController {
+    var temp = 0
+    @IBOutlet weak var CACULATERDisplay: UITextField!
+    
+    @IBAction func button1(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"1"
+    }
+    
+    @IBAction func button2(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"2"
+    }
+    
+    @IBAction func button3(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"3"
+    }
+    
+    @IBAction func buttonCaculater(_ sender: Any) {
+        var sum = 0
+        sum = temp + Int(CACULATERDisplay.text!)!
+        CACULATERDisplay.text = "\(sum)"
+    }
+    
+    @IBAction func buttonAdd(_ sender: Any) {
+        temp = Int(CACULATERDisplay.text!)!
+        CACULATERDisplay.text = ""
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        CACULATERDisplay.text = ""
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
