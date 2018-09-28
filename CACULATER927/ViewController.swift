d1587c90adca96427c0a4801653083d238a5a5ec
20171105135 <1115828778@qq.com>
2018-09-28 09:33:12 +0800

    计算机加减乘除
    增加了加减乘除

--- CACULATER927/ViewController.swift
+++ CACULATER927/ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ViewController:
 UIViewController {
     var temp = 0
+    var flag = 0
     @IBOutlet weak var CACULATERDisplay: UITextField!
     
     @IBAction func button1(_ sender: Any) {
@@ -25,17 +26,80 @@ UIViewController {
         CACULATERDisplay.text = CACULATERDisplay.text!+"3"
     }
     
+    @IBAction func button4(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"4"
+    }
+    
+    @IBAction func button5(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"5"
+    }
+    
+    @IBAction func button6(_ sender: Any) {
+         CACULATERDisplay.text = CACULATERDisplay.text!+"6"
+    }
+    
+    @IBAction func button7(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"7"
+    }
+    
+    @IBAction func button8(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"8"
+    }
+    
+    @IBAction func button9(_ sender: Any) {
+        CACULATERDisplay.text = CACULATERDisplay.text!+"9"        
+    }
+    
     @IBAction func buttonCaculater(_ sender: Any) {
-        var sum = 0
+        if flag==1
+        {
+            var sum = 0
         sum = temp + Int(CACULATERDisplay.text!)!
         CACULATERDisplay.text = "\(sum)"
+        }
+        if flag==2
+        {
+            var subtraction = 0
+            subtraction = temp - Int(CACULATERDisplay.text!)!
+            CACULATERDisplay.text = "\(subtraction)"
+            
+        }
+        if flag==3
+        {
+            var multiplication = 0
+            multiplication = temp * Int(CACULATERDisplay.text!)!
+            CACULATERDisplay.text = "\(multiplication)"
+        }
+        if flag==4
+        {
+            var division = 0
+            division = temp / Int(CACULATERDisplay.text!)!
+            CACULATERDisplay.text = "\(division)"        }
     }
     
     @IBAction func buttonAdd(_ sender: Any) {
         temp = Int(CACULATERDisplay.text!)!
         CACULATERDisplay.text = ""
+        flag=1
     }
     
+    @IBAction func buttonsub(_ sender: Any) {
+        temp = Int(CACULATERDisplay.text!)!
+        CACULATERDisplay.text = ""
+        flag=2
+    }
+    
+    @IBAction func buttonmul(_ sender: Any) {
+        temp = Int(CACULATERDisplay.text!)!
+        CACULATERDisplay.text = ""
+        flag=3
+    }
+    
+    @IBAction func buttondis(_ sender: Any) {
+        temp = Int(CACULATERDisplay.text!)!
+        CACULATERDisplay.text = ""
+        flag=4
+    }
     @IBAction func clear(_ sender: Any) {
         CACULATERDisplay.text = ""
     }
